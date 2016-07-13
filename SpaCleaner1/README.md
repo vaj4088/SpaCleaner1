34c4fe39f1608eccacde8f4f38c08a3a43ae4c05
vaj4088 <ianshef@yahoo.com>
2016-07-12 19:48:23 -0500

    Initial commit based on http://www.vogella.com/tutorials/EclipseGit/article.html

---
+++ SpaCleaner1/README.md
@@ -0,0 +1,2 @@
+# SpaCleaner
+Using an Arduino microprocessor to control two servo motors to operate a portable spa.
---
+++ SpaCleaner1/SpaCleaner1.cpp
@@ -0,0 +1,50 @@
+// Do not remove the include below
+#include "SpaCleaner1.h"
+#include <VarSpeedServo.h>
+//
+// Change these constants to match hardware.
+//
+const byte panelPin = 9;       // The digital pin used for the servo that
+                               // operates the panel on/off button.
+const byte filterPumpPin = 10; // The digital pin used for the servo that
+                               // operates the filter on/off button.
+//
+// Useful constants, do not change.
+//
+const bool WAIT = true ;
+const bool DONTWAIT = false ;
+//
+// Object definitions.
+//
+VarSpeedServo panel ;       // Create a servo object to control the panel servo.
+VarSpeedServo filterPump ;  // Create a servo object to control the
+void setup()
+{
+	  panel.attach(panelPin) ;        // Attaches the panel servo pin
+	                                  // to the panel servo object.
+	  panel.write(0,255,DONTWAIT) ;   // Sets the initial position of
+	                                  // the panel servo,
+	                                  // as fast as possible, run in background
+	  filterPump.attach(filterPumpPin) ; // Attaches the filter pump servo pin
+	                                     // to the filter pump servo object.
+	  filterPump.write(0,255,WAIT) ;     // Sets the initial position of the
+	                                     // filter pump servo,
+	                                     // as fast as possible,
+	                                     // wait until done.
+}
+void loop()
+{
+	//
+    // .write(degrees 0-180, speed 1-255, wait to complete true-false)
+	//
+	  panel.write(180,255,DONTWAIT) ;  // Move the panel servo to 180 degrees,
+	                                   // fast speed, run in background.
+	  filterPump.write(180,255,WAIT) ; // Move the filter pump servo
+	                                   // to 180 degrees,
+	                                   // fast speed, wait until done.
+	  panel.write(0,30,DONTWAIT) ;     // Move the panel servo to 0 degrees,
+	                                   // slow speed, run in background.
+	  filterPump.write(0,30,WAIT) ;    // Move the filter pump servo
+	                                   // to 0 degrees,
+	                                   // slow speed, wait until done.
+}
